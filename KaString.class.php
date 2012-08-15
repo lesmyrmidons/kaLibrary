78ffd0fd91ec0f0519ff0bec3fe0927497a2bd3e
lesmyrmidons <lesmyrmidons@gmail.com>
2012-08-15 18:37:29 +0200

    add class KaString and file test

---
+++ KaString.class.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Cette classe permet de faire tous les traitement
+ * sur une chaine.
+ * @author Kévin ARBOUIN <kevin.arbouin@gmail.com>
+ * @static
+ */
+namespace kaLibrary;
+
+class KaString
+{
+    /**
+     * Checks the validity of an email based on the standards RFC822, RFC2822, RFC1035
+     * Vérifie la validité d'un mail en ce basant sur les normes RFC822, RFC2822, RFC1035
+     * @link	http://atranchant.developpez.com/code/validation/
+     * 
+     * @author Kévin ARBOUIN <kevin.arbouin@gmail.com>
+     * @param	string	$value
+     * @return	boolean
+     * @static
+     */
+    public static function validateEmail($value)
+    {
+        $atom = '[-a-z0-9!#$%&\'*+\\/=?^_`{|}~]';
+        $domain = '([a-z0-9]([-a-z0-9]*[a-z0-9]+)?)';
+
+        $regex = '/^' . $atom . '+' . '(\.' . $atom . '+)*' . '@' . '('
+                . $domain . '{1,63}\.)+' . $domain . '{2,63}$/i';
+
+        if (preg_match($regex, $value)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the string $str every accent delete
+     * Retourne la chaine $str une fois tous les accent supprimer
+     * 
+     * @author Kévin ARBOUIN <kevin.arbouin@gmail.com>
+     * @param    string	$str		String to format
+     * @param    string	$charset	Optional, indicate the charset you want. default is the charset UTF-8
+     * @return   string
+     * @static
+     */
+    public static function removeAccents($str, $charset = 'utf-8')
+    {
+        $str = htmlentities($str, ENT_NOQUOTES, $charset);
+
+        $str = preg_replace(
+                '#&([A-za-z])(?:acute|cedil|circ|grave|orn|ring|slash|th|tilde|uml);#',
+                '\1', $str);
+        // pour les ligatures e.g. '&oelig;'
+        $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str);
+        // supprime les autres caractères
+        $str = preg_replace('#&[^;]+;#', '', trim($str));
+
+        return $str;
+    }
+
+    /**
+     * Returns the file extension based on extensions permitted otherwise returns null
+     * 
+     * @author Kévin ARBOUIN <kevin.arbouin@gmail.com>
+     * @param	string	$fileName
+     * @param	mixed	$ext
+     * @return	string|null
+     * @static
+     */
+    public static function getExtension($fileName, $ext = '/\.[a-zA-Z-]{2,4}$/i')
+    {
+        if (is_array($ext)) {
+            foreach ($ext as $value) {
+                if (preg_match("/{$value}$/", $fileName)) {
+                    return $value;
+                }
+            }
+        } else {
+            preg_match($ext, $fileName, $tab, PREG_OFFSET_CAPTURE);
+            return $tab[0][0];
+        }
+        return null;
+    }
+
+    /**
+     * 
+     * Génère un nom de fichier unique en gardant le nom d'origine et en ajoutant une clé unique
+     * 
+     * @param	string	$filename
+     * @return	string
+     * @author Kévin ARBOUIN <kevin.arbouin@gmail.com>
+     * @static
+     */
+    public static function formatFilename($filename)
+    {
+        $fileExtension = static::getExtension($filename);
+
+        $baseName = substr($filename, 0,
+                strlen($filename) - strlen($fileExtension));
+        $str = strtolower(static::removeAccents($baseName));
+        $str = preg_replace('/ {1,}/', ' ', $str);
+        $stripBaseName = preg_replace('/[^a-zA-Z0-9-]{1,}/', '-', trim($str));
+        $stripBaseName = preg_replace('/[-]{1,}$/', '', trim($stripBaseName));
+
+        $finalFilename = uniqid() . '-' . $stripBaseName . $fileExtension;
+
+        return $finalFilename;
+    }
+}
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php 
+require_once 'KaString.class.php';
+use kaLibrary\KaString;
+
+$filename = " quoize ehgdfk è\"éyé\"' \"èé_ _é\"è'h100%.txt";
+$filename = KaString::formatFilename($filename);
+
+echo $filename . "\n";
+
+$email = 'test@test.fr';
+
+var_dump(KaString::validateEmail($email));
